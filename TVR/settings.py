e994a164b52817e1aa0acc9491d0ccebd3f214c0
danielsurendher <danielsurendher@gmail.com>
2026-08-25 17:50:44 +0530

    Fix MySQL deployment with PyMySQL

--- TVR/settings.py
+++ TVR/settings.py
@@ -79,14 +79,13 @@ WSGI_APPLICATION = 'TVR.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gtec',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
